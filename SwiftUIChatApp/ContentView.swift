e20feb554fa7f8be651a1b5502615c404957d989
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2021-07-29 00:26:05 +0600

    Initial Commit

---
+++ SwiftUIChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChatApp
+//
+//  Created by Shrikanta Mazumder on 29/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIChatApp/SwiftUIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatAppApp.swift
+//  SwiftUIChatApp
+//
+//  Created by Shrikanta Mazumder on 29/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
